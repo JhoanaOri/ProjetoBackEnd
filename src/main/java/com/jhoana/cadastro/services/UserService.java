f39417a685f3a4cb31aae6df9feb32d66b9c3e28
Jhoana Ori <jhoanasousaori@gmail.com>
2021-12-10 22:28:43 -0300

    Service layer, component registration

---
+++ src/main/java/com/jhoana/cadastro/services/UserService.java
@@ -0,0 +1,26 @@
+package com.jhoana.cadastro.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jhoana.cadastro.entities.User;
+import com.jhoana.cadastro.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
